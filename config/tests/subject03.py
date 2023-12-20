0fb259f74aa633607556b20c2ca2ca39f073dc01
Ari Bechtel <aribechtel@MacBook-Pro-3.local>
2023-12-20 12:23:48 -0500

    change test subject03 to 007-004C

--- config/tests/subject03.py
+++ config/tests/subject03.py
@@ -1,4 +1,4 @@
-"""Test config for 002-168, 3D radial ventilation DICOM data."""
+"""Test config for 007-004C, 3D radial ventilation DICOM data."""
 import os
 import sys
 
@@ -18,14 +18,12 @@ class Config(base_config.Config):
         super().__init__()
         self.subject_id = "test"
         self.data_dir = "assets/tests/subject03/"
-        self.xenon_dicom_dir = os.path.join(self.data_dir, "z_recon_xenon")
-        self.proton_dicom_dir = os.path.join(self.data_dir, "z_recon_proton")
-        self.segmentation_key = constants.SegmentationKey.MANUAL_VENT.value
-        self.manual_seg_dir = "assets/tests/subject03/mask_manual.nii"
+        self.xenon_dicom_dir = os.path.join(self.data_dir, "gas_recon_dicoms")
+        self.proton_dicom_dir = os.path.join(self.data_dir, "proton_recon_dicoms")
+        self.segmentation_key = constants.SegmentationKey.CNN_VENT.value
         self.bias_key = constants.BiasfieldKey.N4ITK.value
         self.registration_key = constants.RegistrationKey.SKIP.value
         self.scan_type = constants.ScanType.RADIAL.value
-        self.site = constants.Site.UVA.value
         self.processes = Process()
         self.lock()
 
